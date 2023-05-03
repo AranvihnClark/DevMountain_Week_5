b84f5fe5a1b8b264889a27bb0dba2502c0668e5d
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-05-03 10:39:57 -0400

    Wednesday - Updated parameter names to match instructions

--- Wednesday/chal-pop-growth/popGrowth.js
+++ Wednesday/chal-pop-growth/popGrowth.js
@@ -1,25 +1,25 @@
-const nbYear = (population, rate, augment, goal) => {
+const nbYear = (p0, percent, aug, p) => {
     // To get the rate in %
-    let rateIncrease = rate / 100;
+    let rate = percent / 100;
 
     // You shouldn't reassign/mutate the original parameters, we create a new variable that will act in place of "population".
-    let currentPopulation = population;
+    let currentPopulation = p0;
 
     // To track the number of years
     let years = 0;
 
     // We loop until our current population reaches our goal.
-    while (currentPopulation < goal) {
+    while (currentPopulation < p) {
         
         // The math needed to calcuate our yearly increase in population.
-        currentPopulation += Math.floor((currentPopulation * rateIncrease)) + augment;
+        currentPopulation += Math.floor((currentPopulation * rate)) + aug;
 
         // Then we make sure to add another count to our variable.
         years++;
     }
 
     // We return the answer in a beautiful manner, as if node is talking to us.
-    return console.log(`It took ${years} year(s) to reach the population goal of ${goal}.`);
+    return console.log(`It took ${years} year(s) to reach the population goal of ${p}.`);
 }
 
 nbYear(1500, 5, 100, 5000) // 15
